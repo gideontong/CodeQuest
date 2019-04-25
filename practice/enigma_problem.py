0e2fccd76092833f107af114472bf3865c7562da
Gideon Tong <gideon@gideontong.com>
2019-04-25 13:05:29 -0700

    Try to start something

---
+++ practice/enigma_problem.py
@@ -0,0 +1,9 @@
+# Open the input file
+with open("Prob19.in.txt", "rt") as inputFile:
+    # Read the number of test cases (trim out the newline)
+    cases = int(inputFile.readline().replace("\n", ""))
+    # For each test case
+    for caseNum in range(cases):
+        print("Something")
+
+    
